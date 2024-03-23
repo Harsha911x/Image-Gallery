c29294f9dfbb48f3a5ea6d182b8cfcb7d4c75e72
Harsha911x <149395410+Harsha911x@users.noreply.github.com>
2024-03-23 14:57:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient("https://qblwiojuoomhztfilpug.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFibHdpb2p1b29taHp0ZmlscHVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY4NTMxOTgsImV4cCI6MjAyMjQyOTE5OH0._MxrZXqhf-DYRhvkaxKOHS5F0dEff_I3ScEswuMDTBQ");
+const supabase = createClient("https://wcuojksozvsluyirveli.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjdW9qa3NvenZzbHV5aXJ2ZWxpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTExODU5NjEsImV4cCI6MjAyNjc2MTk2MX0.21aSbuDRlQhncdRM04oGkhtLLm4AmXOSOt9__bzhQPo");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
